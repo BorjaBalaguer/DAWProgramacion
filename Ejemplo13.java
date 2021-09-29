6766ce1bf5fd34e1e75427a3c7a669575f6f9933
BorjaBalaguer <alubal6588@ieselcaminas.org>
2021-09-29 13:42:19 +0200

    Ejercicio13 sin corregir

---
+++ Ejemplo13.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Ejemplo13 {
+
+    public static void main(String[] args) {
+        int valor1, valor2;
+        String orden;
+
+        System.out.println("Pon el primer valor");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        valor1 = inputValue.nextInt();
+
+        System.out.println("Pon el segundo valor");
+        Scanner inputValue2;
+        inputValue2 = new Scanner(System.in);
+        valor2 = inputValue2.nextInt();
+
+        System.out.println("Orden: Ascendente o Descendente?");
+        Scanner inputValue3;
+        inputValue3 = new Scanner(System.in);
+        orden = inputValue3.nextLine();
+
+        if (orden = "Ascendente"){
+            System.out.println("-------------" );
+            if(valor1 > valor2){
+                System.out.println(valor1 );
+                System.out.println(valor2);
+            }else{
+                System.out.println(valor2 );
+                System.out.println(valor1);
+            }
+            
+        }else if (orden = "Descendente"){
+            System.out.println("-------------" );
+            if(valor1 > valor2){
+                System.out.println(valor2 );
+                System.out.println(valor1);
+            }else{
+                System.out.println(valor1 );
+                System.out.println(valor2);
+            }
+        }else{
+            System.out.println("-------------" );
+            System.out.println(valor1 + "=" + valor2);
+        }
+
+
+        inputValue.close();
+        inputValue2.close();
+        inputValue3.close();
+    }
+}
