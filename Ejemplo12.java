160bd8a40df951371375dfded97a742b1b655979
BorjaBalaguer <alubal6588@ieselcaminas.org>
2021-09-29 13:24:20 +0200

    Ejercicio12 corregido

---
+++ Ejemplo12.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Ejemplo12 {
+
+    public static void main(String[] args) {
+        int valor1, valor2;
+
+        System.out.println("Pon el primer valor");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        valor1 = inputValue.nextInt();
+
+        System.out.println("Pon el segundo valor");
+        Scanner inputValue2;
+        inputValue2 = new Scanner(System.in);
+        valor2 = inputValue2.nextInt();
+
+        if (valor1 > valor2){
+            System.out.println("-------------" );
+            System.out.println(valor1 + ">" + valor2);
+        }else if (valor2 > valor1){
+            System.out.println("-------------" );
+            System.out.println(valor2 + ">" + valor1);
+        }else{
+            System.out.println("-------------" );
+            System.out.println(valor1 + "=" + valor2);
+        }
+
+
+        inputValue.close();
+        inputValue2.close();
+    }
+}
