cf409b2a76993229590a03a2e842c350e6f0a4c5
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-04 13:07:54 +0200

    Ej18 menos recursos gastados

--- Ejemplo18.java
+++ Ejemplo18.java
@@ -16,22 +16,23 @@ public class Ejemplo18 {
         int segundos = inputValue.nextInt();
 
         inputValue.close();
-        //Aqui sacamos todos los segundos de los valores dados por el usuario mas el que le añadimos.
-        segundosTotales = (1 + (horas * 3600) + (minutos * 60) + segundos);
-        //Primero sacaremos todas las horas
-        horasTotales = (segundosTotales/3600);
-        //Los segundos restantes los guardaremos en la variable "SegundosRestantesHoras"
-        segundosRestantesHoras = horasTotales * 3600;
-        //Con esos segundo restantes sacaremos los minutos
-        minutosTotales = ((segundosTotales-segundosRestantesHoras)/60);
-        //Y finalmente sacaremos los segundos restantes de los minutos
-        segundosRestantesMinutos = ((segundosTotales-segundosRestantesHoras) - minutosTotales*60);
-
+       
         //Ahora pondremos unas condiciones para no poder poner valores superiores
         if(horas < 24){
             if(minutos < 60){
+              //He puesto todos los calculos en el tercer if, para si la primera condicion esta mal, no gastar recursos.
               if(segundos < 60){
-              System.out.println(horasTotales + ":" + minutosTotales + ":" + segundosRestantesMinutos);
+              //Aqui sacamos todos los segundos de los valores dados por el usuario mas el que le añadimos.
+                segundosTotales = (1 + (horas * 3600) + (minutos * 60) + segundos);
+              //Primero sacaremos todas las horas
+                horasTotales = (segundosTotales/3600);
+              //Los segundos restantes los guardaremos en la variable "SegundosRestantesHoras"
+                segundosRestantesHoras = horasTotales * 3600;
+              //Con esos segundo restantes sacaremos los minutos
+                minutosTotales = ((segundosTotales-segundosRestantesHoras)/60);
+              //Y finalmente sacaremos los segundos restantes de los minutos
+                segundosRestantesMinutos = ((segundosTotales-segundosRestantesHoras) - minutosTotales*60);
+                System.out.println(horasTotales + ":" + minutosTotales + ":" + segundosRestantesMinutos);
               }else{
               System.out.println("Dame unos valores correctos, no puedes dar mas de 23h,59m,59s");
               }
