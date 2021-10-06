8b39513719fcc7f06e0b6c2eae93cbfef6c29f9e
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-06 09:14:59 +0200

    Ej 8 y 9 corregidos con Julio

---
+++ Ejemplo19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejemplo19 {
+    public static void main(String[] args) {
+        
+        int precioH, horas, pago_bruto, pago_neto, impuestos;
+
+        System.out.println("Dame el precio/h");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        precioH = inputValue.nextInt();
+        
+        System.out.println("Dame el numero");
+        horas = inputValue.nextInt();
+
+        
+    }
+}
