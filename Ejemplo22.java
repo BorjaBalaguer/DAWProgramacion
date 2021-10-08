1d63b4a901409c19a8bbfb7911dfe5d3147bd561
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-08 10:20:31 +0200

    Ej 22 acabado

--- Ejemplo22.java
+++ Ejemplo22.java
@@ -1,23 +1,31 @@
+import java.util.Scanner;
+
 public class Ejemplo22 {
     public static void main(String[] args) {
         
-        int i, num, positives;
+        int num, positivos;
 
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        
+        //Inicio de programa
+        System.out.println("Introduce un numero:");
+        num = inputValue.nextInt();
 
-        i = 0;
+        positivos = 0;
+        while (num != 0) {
 
-        while (i < 10) {
+            num = inputValue.nextInt();
 
-    num = miScanner.nextInt();
+            if (num >= 0) {
 
-    if (num >= 0) {
+                positivos = positivos + 1;
 
-        positives = positives + 1;
+            }
+        }
+        inputValue.close();
+        System.out.println("-------------------");
+        System.out.println("Total de numeros positivos: " + positivos);
 
-    }
-
-    i = i + 1;
-
-}
     }
 }
