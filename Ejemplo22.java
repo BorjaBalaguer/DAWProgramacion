1a840180409ccd00a41d9139950ac07d53c649d8
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-08 09:59:13 +0200

    Ej 22 creado

---
+++ Ejemplo22.java
@@ -0,0 +1,23 @@
+public class Ejemplo22 {
+    public static void main(String[] args) {
+        
+        int i, num, positives;
+
+
+        i = 0;
+
+        while (i < 10) {
+
+    num = miScanner.nextInt();
+
+    if (num >= 0) {
+
+        positives = positives + 1;
+
+    }
+
+    i = i + 1;
+
+}
+    }
+}
