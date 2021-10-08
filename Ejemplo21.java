0c7cc340e8a77b1b9406abe6ffe4385a5a508421
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-08 09:41:25 +0200

    Ej 21 acabado

---
+++ Ejemplo21.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejemplo21 {
+     public static void main(String[] args) {
+         
+        int año ;
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        
+        //Inicio de programa
+        System.out.println("Introduce el año:");
+        año = inputValue.nextInt();
+        inputValue.close();
+
+        if ((año % 4 == 0) && ((año % 100 != 0) || (año % 400 == 0))){
+	        System.out.println("El año es bisiesto");
+        }else{
+	        System.out.println("El año no es bisiesto");
+        }
+     }
+}
