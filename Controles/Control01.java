7396a6987e780589641e0da37fe971b0a53bec6a
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-13 09:52:14 +0200

    Control01 acabado

---
+++ Controles/Control01.java
@@ -0,0 +1,28 @@
+package Controles;
+
+import java.util.Scanner;
+
+public class Control01{
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int numLados = 0;
+        double longitudLado = 0;
+        double perimetro = 0;
+
+        System.out.println("Dime el numero de lados del poligono:");
+        numLados = lector.nextInt();
+
+        if ((numLados == 3) || (numLados == 4) )  {
+            System.out.println("Dime la longitud del lado del polígono:");
+            longitudLado = lector.nextInt();
+            perimetro = longitudLado * numLados;
+            if(numLados == 3){
+                System.out.println("El perímetro del triángulo es " + perimetro);
+            }else{
+                System.out.println("El perímetro del cuadrado es" + perimetro);
+            }
+        }
+        lector.close();
+    }
+}
