510a6b602bf4fc4918e0558784c4450c7daa1c3d
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-15 09:55:57 +0200

    Ej 28 acabado

--- Ejemplo26.java
+++ Ejemplo26.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Ejemplo26 {
     public static void main(String[] args) {
-        
+        //Inicializacion de variables
         int numero, multiplicacion = 0, resultado;
         Scanner escaner;
         escaner = new Scanner(System.in);
 
         System.out.println("Escribeme el numero para ver su tabla de multiplicacion: ");
         numero = escaner.nextInt();
-
+        //Inicio programa
         for (int i = 0; i < 11; i++) {
             if(multiplicacion <= 10){
                 resultado = multiplicacion * numero;
---
+++ Ejemplo29.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ejemplo29 {
+    public static void main(String[] args) {
+        
+        int numero, divisor = 2;
+        boolean esPrimo = true;
+
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        System.out.println("Escribeme el numero para saber si es primo o no: ");
+        numero = lector.nextInt();
+
+        while ((divisor < numero) && esPrimo) {
+            if ((numero % divisor) == 0) {
+                esPrimo = false;
+            }
+            divisor ++;
+        }
+        if (esPrimo) {
+            System.out.println("El numero " + numero + " es primo");
+        }else{
+            System.out.println("El numero " + numero + " no es primo");
+        }
+        lector.close();
+    }
+}
