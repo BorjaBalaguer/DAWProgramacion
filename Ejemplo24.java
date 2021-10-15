a6e6ed5610bf14d37562566a6185edcc41e1ee05
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-15 08:39:08 +0200

    Ej 26 acabado

--- Ejemplo24.java
+++ Ejemplo24.java
@@ -1,37 +1,35 @@
-import  java.util.Scanner ;
+import java.util.Scanner;
 
- clase  pública Ejemplo24 {
-    public  static  void  main ( String [] args ) {
+public class Ejemplo24{
+    public static void main(String[] args) {
         
-        int   nota, notaMedia =  0 , suma =  0 , contador =  0 , notaDiez =  0 ;
-        Scanner valorEntrante;
-        valorEntrante =  nuevo  escáner ( System . in);
+        int nota, notaMedia = 0, suma = 0, contador = 0, notaDiez = 0;
+        Scanner lector;
+        lector = new Scanner(System.in);
 
-        
-        Sistema . fuera . println ( " Escribeme tus notas para calcular la media y para finalizar escribe el -1: " );
-        nota = valorEntrante . nextInt ();
+        System.out.println("Escribeme tus notas para calcular la media y para finalizar escribe el -1: ");
+        nota = lector.nextInt();
 
-        while (nota ! =  - 1 ) {
-            contador ++ ;
+        while (nota != -1) {
+            contador ++;
             suma = suma + nota;
             notaMedia = suma / contador;
 
-            si (nota ==  10 ) {
-                notaDiez ++ ;
+            if (nota == 10) {
+                notaDiez ++;
             }
-            
-            nota = valorEntrante . nextInt ();
 
+            nota = lector.nextInt();
         }
 
-        if (notaDiez > =  1 ) {
-            Sistema . fuera . println ( " Habia al menos un 10 entre todas las notas introducidas. " );
-        } más {
-            Sistema . fuera . println ( " NO habia ni un 10 entre todas las notas introducidas. " );
+        if (notaDiez >= 1) {
+            System.out.println("Habia al menos un 10 entre todas las notas introducidas."); 
+        }else{
+            System.out.println("No habia al menos un 10 entre todas las notas introducidas.");
         }
 
-        Sistema . fuera . println ( " La media de las notas es: "  + notaMedia);
+        System.out.println("La media de las notas es: " + notaMedia);
 
-        valorEntrante . cerrar();
-    }    
+        lector.close();
+    }
 }
