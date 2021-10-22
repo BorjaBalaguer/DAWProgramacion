91e31a55acf691dd3cfe3b8af7ff7e8c266b3f86
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-22 09:13:38 +0200

    Ej 33 acabado y Ej 34 empezado

---
+++ Ejemplo33.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejemplo33 {
+    public static void main(String[] args) {
+
+        int numero, resto;
+        char[] anArray = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V',
+                'H', 'L', 'C', 'K', 'E' };
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        System.out.println("Escribe tu DNI sin la letra: ");
+        numero = lector.nextInt();
+
+        resto = numero % 23;
+
+        System.out.println(anArray[resto]);
+
+        lector.close();
+    }
+}
