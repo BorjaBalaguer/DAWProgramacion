a524cf6d79c9ac76caea092bfaccf732a71e738f
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-10-27 09:30:48 +0200

    ArgumentosSuma hecho

--- Tema2/ArgumentosSuma.java
+++ Tema2/ArgumentosSuma.java
@@ -1,11 +1,18 @@
-public class ArgumentosMain {
+public class ArgumentosSuma {
     public static void main(String[] args) {
+
+        //Declaración de variables
+        int suma = 0;
+
         System.out.println("--------------------------");
         System.out.println("Argumentos: " + args.length);
         System.out.println("--------------------------");
+
         for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+            int temp = Integer.parseInt(args[i]);
+            suma += temp;
         }
+        System.out.println(suma);
         System.out.println("--------------------------");
     }
 }
