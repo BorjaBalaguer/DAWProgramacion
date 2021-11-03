a9b662d96885f630fd9b5959413930e07b16c2d2
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-03 09:32:37 +0100

    Ej 35 corregido

--- Tema1/Ejemplo35.java
+++ Tema1/Ejemplo35.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 public class Ejemplo35 {
     public static void main(String[] args) {
-        int[] anArray;
-        int total;
+        int[] notas;
+        double total = 0;
         Scanner lector;
+        boolean hayDiez = false;
         lector = new Scanner(System.in);
 
-        anArray = new int[10];
+        notas = new int[10];
 
         System.out.println("Dame 10 numeros: ");
-        for (int i = 0; i < anArray.length ;i++) {
-            anArray[i] = lector.nextInt();
+        for (int i = 0; i < notas.length ;i++) {
+            notas[i] = lector.nextInt();
+            total += notas[i];
         }
-        total = (anArray[0]);
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] == 10) {
+                hayDiez = true;
+            }
+        }
+        System.out.println("--------------------------");
+        if (hayDiez) {
+            System.out.println("Hay almenos un diez en las notas introducidas");
+        }
+        System.out.println("La media es igual a: " +(total/10));
         lector.close();
     }
 }
