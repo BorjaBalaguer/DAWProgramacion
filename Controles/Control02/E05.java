7e6af3378d54330ed0537bf11de88148d9186c43
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-05 09:33:05 +0100

    E05 sin acabar

---
+++ Controles/Control02/E05.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class E05 {
+    public static void main(String[] args) {
+        int numero = 0;
+        Scanner lector = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+        numero = lector.nextInt();
+    }
+}
