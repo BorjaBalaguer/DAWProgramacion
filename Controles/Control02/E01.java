5aab5fc126bb8d0073ed9709744ee05328b423e1
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-05 09:06:50 +0100

    E01 acabado corregido

---
+++ Controles/Control02/E01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class E01 {
+   public static void main(String[] args) {
+       String nombre;
+       Scanner lector = new Scanner(System.in);
+       
+        System.out.print("Ingrese su nombre: ");
+        nombre = lector.nextLine();
+        System.out.println("Hola, " + nombre);
+   } 
+}
