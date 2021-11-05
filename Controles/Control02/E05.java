a121b9882b7635786fe9d30e9248ad61882a9427
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-05 10:31:56 +0100

    E05 acabado corregido

--- Controles/Control02/E05.java
+++ Controles/Control02/E05.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 
 public class E05 {
     public static void main(String[] args) {
-        int numero = 0;
+        int numero = 0, invertido = 0, resto = 0;
         Scanner lector = new Scanner(System.in);
 
-        System.out.println("Ingrese un numero: ");
+        System.out.print("Ingrese un numero: ");
         numero = lector.nextInt();
-        lector.close();
+
+        while( numero > 0 ) {
+            //Lo que hace esta formula es ir sacando numeros de derecha y los va poniendo a la izquierda, hasta que se queda sin valores
+            resto = numero % 10;
+            invertido = invertido * 10 + resto;
+            numero /= 10;
+        }
+        System.out.println(invertido );
+        lector.close(); 
     }
 }
---
+++ Controles/Control02/E08.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class E08 {
+    public static void main(String[] args) {
+        int numero = 0, decimal = 0;
+        Scanner lector = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+        numero = lector.nextInt();
+
+    }
+}
