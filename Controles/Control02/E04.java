65c7875aef98419f3eabd7476429dc84829c8012
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-05 09:28:37 +0100

    E04 acabado corregido

---
+++ Controles/Control02/E04.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class E04 {
+    public static void main(String[] args) {
+        int longitud = 0; 
+        double pulgadas = 0;
+        Scanner lector = new Scanner(System.in);
+       
+        System.out.print("Ingrese longitud: ");
+        longitud = lector.nextInt();
+
+        pulgadas = longitud * 0.39370;
+        System.out.println(longitud + " cm = " + pulgadas + " in");
+    }
+}
