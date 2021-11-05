85634b3e8a1174aec2ad7309f54c6eacae52c4db
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-05 10:40:46 +0100

    E08 sin acabar

--- Controles/Control02/E08.java
+++ Controles/Control02/E08.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class E08 {
     public static void main(String[] args) {
-        int numero = 0, decimal = 0;
+        double numero = 0, decimal = 0;
         Scanner lector = new Scanner(System.in);
 
         System.out.println("Ingrese un numero: ");
         numero = lector.nextInt();
 
+        decimal = decimal - Math.trunc(numero);
+
+        System.out.println(decimal);
     }
 }
