ee6ae285885f08c73cfbb9ea271edf7fc3186e7b
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-11-12 10:41:45 +0100

    Positivos hecho

--- Tema2/Ejercicios/Positivos.java
+++ Tema2/Ejercicios/Positivos.java
@@ -1,5 +1,24 @@
 public class Positivos {
-    public static (String[] args) {
-        
+
+    public static int positivos(int [] numeros) {
+        int resultado = 0;
+
+        for (int i=0; i < numeros.length; i++) {
+            if (numeros[i] >= 0) {
+                resultado++;
+            }
+        }
+
+        return resultado;
+    }
+    public static void main(String[] args) {
+        int [] numeritos = {4, 6, -1, -2, 5, 9}; 
+        System.out.println(positivos(numeritos)); 
+
+        int [] numeritos2 = {-1, -2}; 
+        System.out.println(positivos(numeritos2)); 
+
+        int [] numeritos3 = {}; 
+        System.out.println(positivos(numeritos3)); 
     }
 }
