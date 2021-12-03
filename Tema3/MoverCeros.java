e1720a64a0f95b8719fbee65a4cd18a88227fddd
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2021-12-03 08:26:13 +0100

    Ej MoverCeros acabado

--- Tema3/MoverCeros.java
+++ Tema3/MoverCeros.java
@@ -1,14 +1,24 @@
+import java.util.Arrays;
+
 public class MoverCeros {
     public static void main(String[] args) {
         //int[] numeros = new int [10];
         //Utilidades.rellenaArray(numeros, 1, 15);
         int[] numeros = {1,2,0,3,0,4};
 
-        for (int i = (numeros.length -1) ; i > 0; i--) {
+        for (int i = (numeros.length -1) ; i >= 0; i--) {
             if (numeros[i] == 0) {
-                numeros[numeros.length] = numeros[i]; 
-                numeros[i] = numeros[i+1];
+                moverCeroDerecha(numeros, i);
             }
         }
+        System.out.println("Array final: " + Arrays.toString(numeros));
      }
+
+    public static void moverCeroDerecha(int[] numeros, int pos) {
+
+        for (int i = pos; i < numeros.length -1; i++) {
+            numeros[i] = numeros[i +1];
+            numeros[i +1] = 0;
+        }
+    }
 }
