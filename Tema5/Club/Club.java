6cc74e023f6f8017ddae70d24262e976102175c5
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2022-03-04 09:15:15 +0100

    Ejercicio Club acabado

--- Tema5/Club/Club.java
+++ Tema5/Club/Club.java
@@ -1,5 +1,8 @@
 package Tema5.Club;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Store details of club memberships.
  * 
@@ -9,6 +12,8 @@ package Tema5.Club;
 public class Club
 {
     // Define any necessary fields here ...
+
+    private static ArrayList<Membership> miembrosclub = new ArrayList<>();
     
     /**
      * Constructor for objects of class Club
@@ -25,6 +30,7 @@ public class Club
      */
     public void join(Membership member)
     {
+        miembrosclub.add(member);
     }
 
     /**
@@ -33,6 +39,36 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return 0;
+        return miembrosclub.size();
+    }
+
+    public int joinedlnMonth(int month){
+        int contadorMiembros = 0;
+
+        if (month >= 1 & month <= 12) {
+            for (Membership memberships : miembrosclub) {
+                if (memberships.getMonth() == month) {
+                    contadorMiembros++;
+                }
+            }
+        }else{
+            System.out.println("El mes no existe");
+        }
+
+        return contadorMiembros;
+    }
+
+    public int purge(int month){
+        Iterator<Membership> it = miembrosclub.iterator();
+        int miembrosEliminados= 0;
+
+        while(it.hasNext()){
+            Membership iMembership = it.next();
+            if (iMembership.getMonth() == month) {
+                it.remove();
+                miembrosEliminados++;
+            }
+        }
+        return miembrosEliminados;
     }
 }
