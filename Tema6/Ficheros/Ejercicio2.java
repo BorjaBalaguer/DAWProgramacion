b782447c08e979cbbdae8aaa049e4dfb271decb1
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2022-04-01 09:28:49 +0200

    Ejercicio1 acabado

--- Tema6/Ficheros/Ejercicio2.java
+++ Tema6/Ficheros/Ejercicio2.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Ejercicio1 {
+public class Ejercicio2 {
 
     private static void imprimirFichero(File e, int id) {
 
