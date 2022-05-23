b3071c7e896807bebdd5b6cb845f5a0d1df1449e
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2022-05-23 08:57:43 +0200

    Cliente cambiado

--- Controles/Control04/PersistenciaCliente.java
+++ Controles/Control04/PersistenciaCliente.java
@@ -2,15 +2,12 @@ package Controles.Control04;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
-import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class PersistenciaCliente {
     
