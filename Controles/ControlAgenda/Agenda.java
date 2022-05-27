d6a72fd43b70831a82d9d0fdfd1fd0708a3b0e6a
BorjaBalaguercd <alubal6588@ieselcaminas.org>
2022-05-27 09:27:58 +0200

    Agenda corregido

---
+++ Controles/ControlAgenda/Agenda.java
@@ -0,0 +1,90 @@
+package Controles.ControlAgenda;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Agenda {
+
+    private final String nombreFicheroTexto = "Controles/ControlAgenda/agenda.dat";
+    private final String nombreFicheroObjetos = "Controles/ControlAgenda/agendaObjetos.dat";
+
+    private Scanner lector;
+    
+    private FileWriter ficheroTexto;
+    private PrintWriter escritorTexto;
+
+    private FileOutputStream ficheroObjetos;
+    private ObjectOutputStream escritorObjetos;
+
+    public Agenda() {
+        this.lector = new Scanner(System.in);
+
+        try {
+            this.ficheroTexto = new FileWriter(nombreFicheroTexto,true);
+            escritorTexto = new PrintWriter(this.ficheroTexto);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            this.ficheroObjetos = new FileOutputStream(nombreFicheroObjetos, true);
+            this.escritorObjetos = new ObjectOutputStream(this.ficheroObjetos);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public Usuario pedirUsuario() {
+        Usuario nuevoUsuario = new Usuario();
+        
+        System.out.println("Nombre: ");
+        nuevoUsuario.setNombre(lector.nextLine());
+
+        System.out.println("Apellidos: ");
+        nuevoUsuario.setApellidos(lector.nextLine()); 
+
+        System.out.println("Teléfono: ");
+        nuevoUsuario.setTelefono(lector.nextLine()); 
+
+        System.out.println("Email: ");
+        nuevoUsuario.setEmail(lector.nextLine());
+
+        this.escritorTexto.println(nuevoUsuario);
+
+        try {
+            this.escritorObjetos.writeObject(nuevoUsuario);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return nuevoUsuario;
+    }
+
+    public void imprimirListadoTexto() {
+        
+    }
+
+    public void imprimirListadoObjetos() {
+        
+    }
+
+    public void terminar(){
+        try {
+            this.escritorObjetos.close();
+            this.ficheroObjetos.close();
+            this.ficheroTexto.close();
+            this.ficheroObjetos.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
